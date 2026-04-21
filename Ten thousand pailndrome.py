9437257222adc011e41f5d5da5d794aff18730d8
Kunal Farkade <farkadekunal@gmail.com>
2026-04-22 05:25:07 +0530

    Add files via upload

---
+++ Ten thousand pailndrome.py
@@ -0,0 +1,11 @@
+for num in range(1, 10001):
+    temp = num
+    rev = 0
+
+    while temp > 0:
+        rem = temp % 10
+        rev = rev * 10 + rem
+        temp = temp // 10
+
+    if num == rev:
+        print(num)
